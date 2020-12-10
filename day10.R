34cf78769fadb3399ac854db8007a56a97e4e84f
Maria Paula Caldas <33395215+mpaulacaldas@users.noreply.github.com>
2020-12-10 20:54:15 +0100

    Solve part 2, day 10

--- day10.R
+++ day10.R
@@ -1,14 +1,45 @@
-input <- as.numeric(readLines("data-raw/day10.txt"))
-sorted <- sort(input)
-
-# question 1
-diffs <- c(sorted - dplyr::lag(sorted, default = 0), 3)
-Reduce(`*`, table(diffs))
-
-# question 2
 library(purrr)
 
-pos <- sorted %>%
-  set_names() %>%
-  map(~ intersect(.x - 1:3, sorted)) %>%
-  compact() # TOOD: think
+input <- as.numeric(readLines("data-raw/day10.txt"))
+
+# question 1 --------------------------------------------------------------
+
+lead <- c(sort(input), max(input) + 3)
+lag  <- c(0, lead[-length(lead)])
+
+diffs <- lead - lag
+reduce(table(diffs), `*`)
+
+
+# question 2 --------------------------------------------------------------
+
+# if the input is valid, return the sorted input, if not, NULL
+give_valid_arr <- function(x) {
+  x <- sort(x)
+  lead <- c(x, max(input) + 3) # max(input), NOT max(x)
+  lag  <- c(0, lead[-length(lead)])
+  diff <- lead - lag
+  if (all(diff %in% 1:3)) x
+}
+
+# for a given input, return all combinations that drop one of the numbers, check
+# and return the valid sequences
+n_valid_arr_rem_1 <- function(x) {
+  x %>%
+    seq_along() %>%
+    map(~ give_valid_arr(x[-.x])) %>%
+    compact()
+}
+
+reduced <- n_valid_arr_rem_1(input)
+count   <- 1 + length(reduced)
+
+while (length(reduced) > 1) {
+  reduced <- reduced %>%
+    map(n_valid_arr_rem_1) %>%
+    unlist(recursive = FALSE) %>%
+    unique()
+  count <- count + length(reduced)
+}
+
+count
